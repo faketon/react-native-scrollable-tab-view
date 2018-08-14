d0f52de172e629f4756ca25f0b3fa062ed0c88d5
faketon <chalernponman@hotmail.com>
2018-08-14 14:11:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 
-const ScrollableTabView = React.createClass({
+const ScrollableTabView = createReactClass({
   mixins: [TimerMixin, ],
   statics: {
     DefaultTabBar,
